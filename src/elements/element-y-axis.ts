a2213336dde4c1b5d189fdf9d71e37e5a7f2b0c7
johnwalley <john@walley.org.uk>
2021-02-23 10:04:16 +0000

    feat: fixed width y axes

--- src/elements/element-y-axis.ts
+++ src/elements/element-y-axis.ts
@@ -3,41 +3,53 @@ import { ScaleLinear, ScaleTime } from "d3-scale";
 import { Colors } from "../helpers";
 import { Element } from "../types/element";
 
+const WIDTH = 50;
+const MARGIN = 6;
+
 function addYAxisPath(
   ctx: CanvasRenderingContext2D,
   xScale: ScaleTime<number, number, never>,
   yScale: ScaleLinear<number, number, never>
 ) {
-  ctx.strokeStyle = "#fff";
-
+  const xRange = xScale.range();
   const yRange = yScale.range();
   const numYTicks = Math.abs(yRange[1] - yRange[0]) / 60;
   const yTicks = yScale.ticks(numYTicks);
 
+  ctx.beginPath();
+  ctx.fillStyle = "rgba(0,0,0,0.6)";
+
+  ctx.fillRect(
+    xScale.range()[1] - WIDTH,
+    yRange[1],
+    WIDTH,
+    yRange[0] - yRange[1]
+  );
+
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.strokeStyle = Colors.GRAY_LIGHT;
+  ctx.moveTo(xRange[1] - WIDTH, yRange[0]);
+  ctx.lineTo(xRange[1] - WIDTH, yRange[1]);
+  ctx.stroke();
+  ctx.closePath();
+
+  ctx.strokeStyle = "#fff";
+  ctx.fillStyle = Colors.GRAY_LIGHT;
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "left";
+  ctx.font = `12px monospace`;
+
   yTicks.forEach((tick: number) => {
-    const text = tick.toString();
-    const textWidth = ctx.measureText(text).width;
-
     ctx.beginPath();
-    ctx.fillStyle = "rgba(0,0,0,0.8)";
-    ctx.fillRect(
-      xScale.range()[1] - textWidth - 10,
-      yScale(tick) - 10,
-      textWidth + 10,
-      20
-    );
-    ctx.closePath();
 
-    ctx.beginPath();
-    ctx.fillStyle = Colors.GRAY_LIGHT;
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "right";
-    ctx.font = `12px monospace`;
     ctx.fillText(
       String(Math.round(tick)),
-      xScale.range()[1] - 5,
+      xRange[1] - WIDTH + MARGIN,
       Math.round(yScale(tick))
     );
+
     ctx.closePath();
   });
 }
