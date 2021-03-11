797606e2c7ba1671e3c68eea2544d4890ed80353
johnwalley <john@walley.org.uk>
2021-03-11 09:57:14 +0000

    chore: rename chart stories

--- src/components/chart.stories.tsx
+++ src/components/chart.stories.tsx
@@ -1,9 +1,8 @@
-import { Chart, ChartProps } from "../components/chart";
+import { Chart, ChartProps } from "./chart";
 import { Meta, Story } from "@storybook/react/types-6-0";
 
 import { Interval } from "../api/vega-graphql";
 import { JsonDataSource } from "../data/json-data-source";
-import React from "react";
 
 export default {
   title: "Components/Chart",
