06251eba3a2f81dd026e2973eeed6a248f0090b8
johnwalley <john@walley.org.uk>
2021-03-26 17:45:53 +0000

    build: add @babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+  ],
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
