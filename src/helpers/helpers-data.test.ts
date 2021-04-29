b6f7685195455edf3dfe8eda8290ac29ca24e339
johnwalley <john@walley.org.uk>
2021-04-29 21:48:50 +0100

    test: fix up data tests

--- src/helpers/helpers-data.test.ts
+++ src/helpers/helpers-data.test.ts
@@ -1,54 +1,88 @@
 import { mergeData } from "./helpers-data";
 import { range } from "d3-array";
-
-type Datum = { date: Date };
+import { Candle } from "../types";
 
 const arrayA = range(10).map((d) => new Date(2021, 1, d));
 const arrayC = range(10).map((d) => new Date(2021, 1, d + 5));
 
 describe("mergeData", () => {
   it("merges empty arrays", () => {
-    const a: Datum[] = [];
-    const b: Datum[] = [];
+    const a: Candle[] = [];
+    const b: Candle[] = [];
     expect(mergeData(a, b)).toEqual([]);
   });
 
   it("merges with second array empty", () => {
-    const a: Datum[] = arrayA.map((d) => ({
+    const a: Candle[] = arrayA.map((d) => ({
       date: d,
+      open: 0,
+      high: 0,
+      low: 0,
+      close: 0,
+      volume: 0,
     }));
-    const b: Datum[] = [];
+    const b: Candle[] = [];
     expect(mergeData(a, b)).toEqual(
       arrayA.map((d) => ({
         date: d,
+        open: 0,
+        high: 0,
+        low: 0,
+        close: 0,
+        volume: 0,
       }))
     );
   });
 
   it("merges with first array empty", () => {
-    const a: Datum[] = arrayA.map((d) => ({
+    const a: Candle[] = arrayA.map((d) => ({
       date: d,
+      open: 0,
+      high: 0,
+      low: 0,
+      close: 0,
+      volume: 0,
     }));
-    const b: Datum[] = [];
+    const b: Candle[] = [];
     expect(mergeData(b, a)).toEqual(
       arrayA.map((d) => ({
         date: d,
+        open: 0,
+        high: 0,
+        low: 0,
+        close: 0,
+        volume: 0,
       }))
     );
   });
 
   it("merges overlapping arrays", () => {
-    const a: Datum[] = arrayA.map((d) => ({
+    const a: Candle[] = arrayA.map((d) => ({
       date: d,
+      open: 0,
+      high: 0,
+      low: 0,
+      close: 0,
+      volume: 0,
     }));
-    const b: Datum[] = arrayC.map((d) => ({
+    const b: Candle[] = arrayC.map((d) => ({
       date: d,
+      open: 0,
+      high: 0,
+      low: 0,
+      close: 0,
+      volume: 0,
     }));
     expect(mergeData(a, b)).toEqual(
       range(15)
         .map((d) => new Date(2021, 1, d))
         .map((d) => ({
           date: d,
+          open: 0,
+          high: 0,
+          low: 0,
+          close: 0,
+          volume: 0,
         }))
     );
   });
