0ffed561eb632cf65182bc656dbdc913b914264d
johnwalley <john@walley.org.uk>
2021-05-10 10:44:58 +0100

    refactor: move studyInfoFields into helpers

--- src/components/plot-container/plot-container.tsx
+++ src/components/plot-container/plot-container.tsx
@@ -24,41 +24,7 @@ import { throttle } from "lodash";
 import { THROTTLE_INTERVAL, WIDTH } from "../../constants";
 import { CloseButton } from "./close-button";
 import { Core } from "../../core";
-import { getStudyInfoFieldValue } from "./helpers";
-
-const studyInfoFields: Record<
-  string,
-  { label: string; fields: { id: string; label: string }[] }
-> = {
-  main: {
-    label: "Candle",
-    fields: [
-      { id: "open", label: "O" },
-      { id: "high", label: "H" },
-      { id: "low", label: "L" },
-      { id: "close", label: "C" },
-    ],
-  },
-  eldarRay: {
-    label: "Eldar-ray",
-    fields: [
-      { id: "bullPower", label: "Bull" },
-      { id: "bearPower", label: "Bear" },
-    ],
-  },
-  macd: {
-    label: "MACD",
-    fields: [
-      { id: "signal", label: "S" },
-      { id: "divergence", label: "D" },
-      { id: "macd", label: "MACD" },
-    ],
-  },
-  volume: {
-    label: "Volume",
-    fields: [{ id: "volume", label: "V" }],
-  },
-};
+import { getStudyInfoFieldValue, studyInfoFields } from "./helpers";
 
 export type PlotContainerProps = {
   width: number;
