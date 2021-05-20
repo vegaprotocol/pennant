088079662bdd11e898aa35b8ea54fb0e6f86748c
johnwalley <john@walley.org.uk>
2021-05-20 20:15:02 +0100

    chore: add calculateShiftedPositions test

--- src/helpers/helpers-math.ts
+++ src/helpers/helpers-math.ts
@@ -1,14 +1,12 @@
 export function calculateShiftedPositions(
-  yPositions: number[],
+  positions: number[],
   height: number
 ) {
   let previousY = -Infinity;
-
-  const sortedYPositions = [...yPositions].sort((a, b) => a - b);
+  const sortedYPositions = [...positions].sort((a, b) => a - b);
 
   const shiftedYPositions = sortedYPositions.reduce<number[]>((p, y) => {
     const ypx = y;
-
     let ny = ypx;
 
     if (ypx - previousY < height) {
