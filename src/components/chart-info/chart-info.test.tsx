bfaae9821287cb92aa1eee7d05c010e37793c8f6
johnwalley <john@walley.org.uk>
2021-05-20 20:32:21 +0100

    test: add chart-info snapshot test

---
+++ src/components/chart-info/chart-info.test.tsx
@@ -0,0 +1,12 @@
+import renderer from "react-test-renderer";
+
+import { ChartInfo } from ".";
+
+test("ChartInfo renders correctly", () => {
+  const component = renderer.create(
+    <ChartInfo bounds={[new Date(2021, 2, 1), new Date(2021, 2, 2)]} />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
