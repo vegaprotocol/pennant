ba864c4333628385c206bbbab201e92b99f99d9d
johnwalley <john@walley.org.uk>
2021-06-10 22:32:43 +0100

    build: address babel error when building storybook

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-  ],
+  plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
