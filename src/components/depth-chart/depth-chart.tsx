140fffd52ca01581b0b74b4958797937f6dcf4d4
johnwalley <john@walley.org.uk>
2021-10-27 21:33:26 +0100

    chore: better fallback depth chart colors

--- src/components/depth-chart/depth-chart.tsx
+++ src/components/depth-chart/depth-chart.tsx
@@ -39,22 +39,22 @@ const cssStyleDeclaration = getComputedStyle(document.documentElement);
 
 export const BUY_FILL = string2hex(
   cssStyleDeclaration.getPropertyValue("--pennant-color-buy-fill").trim() ||
-    "#070c07"
+    "#16452d"
 );
 
 export const BUY_STROKE = string2hex(
   cssStyleDeclaration.getPropertyValue("--pennant-color-buy-stroke").trim() ||
-    "#5a9c55"
+    "#26ff8a"
 );
 
 export const SELL_FILL = string2hex(
   cssStyleDeclaration.getPropertyValue("--pennant-color-sell-fill").trim() ||
-    "#2b0d13"
+    "#800700"
 );
 
 export const SELL_STROKE = string2hex(
   cssStyleDeclaration.getPropertyValue("--pennant-color-sell-stroke").trim() ||
-    "#da3f5b"
+    "#ff261a"
 );
 
 export const GRAY = string2hex(Colors.GRAY);
