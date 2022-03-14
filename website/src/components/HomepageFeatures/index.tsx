128cb20bdabcb4759d06d3bbeee82f009128df62
johnwalley <john@walley.org.uk>
2022-03-14 11:38:54 +0000

    feat: theme more like Vega Fairground

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -5,19 +5,19 @@ import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  src: string;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
     title: "High performance",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    src: require("@site/static/img/Auto-Ghost-Only.png").default,
     description: <>Built on the Canvas API and optimized to render at 60fps.</>,
   },
   {
     title: "Technical indicators",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    src: require("@site/static/img/Spinning-Plates.png").default,
     description: (
       <>
         Includes technical indicators including Bollinger bands, EMA, and MACD.
@@ -27,16 +27,16 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "Open source and extensible",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    src: require("@site/static/img/Clown-and-Coffee-Ghost.png").default,
     description: <>MIT licensed and accepting community contributions.</>,
   },
 ];
 
-function Feature({ title, Svg, description }: FeatureItem) {
+function Feature({ title, src, description }: FeatureItem) {
   return (
     <div className={clsx("col col--4")}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img className={styles.featureSvg} src={src} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
