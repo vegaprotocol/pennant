4613559c25ecef3ae041f128f511d30d610458b1
johnwalley <john@walley.org.uk>
2022-03-28 15:43:08 +0100

    chore: export chart component props types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export type { ChartProps } from "./components/chart";
 export { Chart } from "./components/chart";
+export type {
+  DepthChartHandle,
+  DepthChartProps,
+} from "./components/depth-chart";
 export { DepthChart } from "./components/depth-chart";
 export * from "./types";
