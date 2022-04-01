c52f957344850ef39dd6960792b3b79220698b84
johnwalley <john@walley.org.uk>
2022-04-01 14:54:11 +0100

    fix: depth chart should not error outside browser

--- src/components/depth-chart/helpers/helpers-color.ts
+++ src/components/depth-chart/helpers/helpers-color.ts
@@ -10,42 +10,43 @@ export interface Colors {
   textSecondary: number;
 }
 
-export function getColors(element: HTMLElement): Colors {
-  const cssStyleDeclaration = getComputedStyle(element);
+export function getColors(element: HTMLElement | null): Colors {
+  const cssStyleDeclaration = element ? getComputedStyle(element) : null;
 
   return {
     buyFill: string2hex(
-      cssStyleDeclaration.getPropertyValue("--pennant-color-buy-fill").trim() ||
-        "#16452d"
+      cssStyleDeclaration
+        ?.getPropertyValue("--pennant-color-buy-fill")
+        .trim() || "#16452d"
     ),
     buyStroke: string2hex(
       cssStyleDeclaration
-        .getPropertyValue("--pennant-color-buy-stroke")
+        ?.getPropertyValue("--pennant-color-buy-stroke")
         .trim() || "#26ff8a"
     ),
     sellFill: string2hex(
       cssStyleDeclaration
-        .getPropertyValue("--pennant-color-sell-fill")
+        ?.getPropertyValue("--pennant-color-sell-fill")
         .trim() || "#800700"
     ),
     sellStroke: string2hex(
       cssStyleDeclaration
-        .getPropertyValue("--pennant-color-sell-stroke")
+        ?.getPropertyValue("--pennant-color-sell-stroke")
         .trim() || "#ff261a"
     ),
     textPrimary: string2hex(
       cssStyleDeclaration
-        .getPropertyValue("--pennant-font-color-base")
+        ?.getPropertyValue("--pennant-font-color-base")
         .trim() || "#ffffff"
     ),
     textSecondary: string2hex(
       cssStyleDeclaration
-        .getPropertyValue("--pennant-font-color-secondary")
+        ?.getPropertyValue("--pennant-font-color-secondary")
         .trim() || "#fafafa"
     ),
     backgroundSurface: string2hex(
       cssStyleDeclaration
-        .getPropertyValue("--pennant-background-surface-color")
+        ?.getPropertyValue("--pennant-background-surface-color")
         .trim() || "#0a0a0a"
     ),
   };
