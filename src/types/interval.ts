bf63a1f2c5002b055fa3504d6f180d81cd9b41ed
johnwalley <john@walley.org.uk>
2022-05-04 12:06:08 +0100

    feat: export interval labels

--- src/types/interval.ts
+++ src/types/interval.ts
@@ -1,11 +1,19 @@
-/**
- * The time interval for trade candles
- */
-export enum Interval {
-  I15M = "I15M",
-  I1D = "I1D",
-  I1H = "I1H",
-  I1M = "I1M",
-  I5M = "I5M",
-  I6H = "I6H",
-}
+export const Interval = {
+  I1M: "I1M",
+  I5M: "I5M",
+  I15M: "I15M",
+  I1H: "I1H",
+  I6H: "I6H",
+  I1D: "I1D",
+} as const;
+
+export type Interval = typeof Interval[keyof typeof Interval];
+
+export const intervalLabels: Record<Interval, string> = {
+  I1M: "1m",
+  I5M: "5m",
+  I15M: "15m",
+  I1H: "1H",
+  I6H: "6H",
+  I1D: "1D",
+} as const;
