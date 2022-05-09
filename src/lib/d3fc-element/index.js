36a966f2a1d48456b780d1e8482f0be5d7741ffa
John Walley <john@walley.org.uk>
2022-05-09 13:59:16 +0100

    fix: remove log messages when attempting to register custom elements (#460)

--- src/lib/d3fc-element/index.js
+++ src/lib/d3fc-element/index.js
@@ -6,14 +6,9 @@ import Group from "./src/group";
 import Svg from "./src/svg";
 
 if (
-  typeof customElements !== "object" ||
-  typeof customElements.define !== "function"
+  typeof customElements === "object" &&
+  typeof customElements.define === "function"
 ) {
-  console.warn(
-    "d3fc-element depends on Custom Elements (v1). Make sure that you load a polyfill in older browsers. See README."
-  );
-} else {
-  console.info("Registered custom elements");
   customElements.define("d3fc-canvas", Canvas);
   customElements.define("d3fc-group", Group);
   customElements.define("d3fc-svg", Svg);
