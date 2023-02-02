1f9959e7006ad3469dd76f4a3ae93efaadfc63db
johnwalley <john@walley.org.uk>
2023-02-02 13:36:21 +0000

    chore: extract string helpers

--- src/components/depth-chart/helpers/helpers-dimensions.ts
+++ src/components/depth-chart/helpers/helpers-dimensions.ts
@@ -1,25 +1,4 @@
-/**
- * Checks if `string` ends with the given target string.
- */
-export function endsWith(string: string, target: string): boolean {
-  const length = string.length;
-
-  const position = length - target.length;
-
-  return position >= 0 && string.slice(position, length) === target;
-}
-
-function string2num(string: string, fallback = 1) {
-  let width = fallback;
-
-  if (endsWith(string, "px")) {
-    width = Number(string.slice(0, -2));
-  } else if (typeof Number.parseFloat(string) === "number") {
-    width = Number.parseFloat(string);
-  }
-
-  return width;
-}
+import { string2num } from "../../../helpers/helpers-string";
 
 export interface Dimensions {
   strokeWidth: number;
